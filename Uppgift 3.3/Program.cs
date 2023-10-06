23032305fe38a1df3858b56c2842eae327d0be34
hast06001 <hast06001@E-PW024NW1>
2023-10-06 11:09:50 +0200

    Add project files.

---
+++ Uppgift 3.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur länge vill du hyra bilen? (skriv i hela timmar)");
+            int timmar = int.Parse(Console.ReadLine());
+
+            int pris = timmar * 80;
+
+            if (pris > 950)
+                Console.WriteLine("Det kommer att kosta 950kr");
+            else
+                Console.WriteLine($"Det kommer att kosta {pris}kr.");
+
+            Console.ReadKey();
+        }
+    }
+}
